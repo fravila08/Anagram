bd6e18ea9dd7038fe912464b6acb82abc8c68560
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-28 00:39:50 -0700

    py done

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,15 @@
 # Don't forget to run the tests (and create some of your own)
 
+
 def anagrams_for(word, list_of_words):
-		# your code here
+    anagrams=[]
+    for i in list_of_words:
+        counter= 0
+        for j in word:
+            compare_one=i.count(j)
+            compare_two=word.count(j)
+            if compare_one == compare_two:
+                counter += 1
+        if counter==len(i):
+            anagrams.append(i)
+    return anagrams
